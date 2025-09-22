0b172b4c8c41a371b3ab1940e11e8423bbe0705a
Charan <charan@gmail.com>
2025-09-22 20:56:49 +0530

    some message

---
+++ 03-Variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+$PERSON1=Jyoo
+$PERSON2=Tej
+
+echo "$PERSON1:: Hi, $PERSON2, How are you?"
+echo "$PERSON2:: Hello $PERSON1, I am fine, How are you?"
+echo "$PERSON1:: What are you doing recently?"
+echo "$PERSON2:: I am learning shell scripting $PERSON1"
