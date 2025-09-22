c6c785fce7a250f87a2f9cc158cf6c0a8a398dbe
Charan <charan@gmail.com>
2025-09-22 20:50:50 +0530

    Second commit

---
+++ 02-Conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Jyoshna:: Hi, Charan, How are you?"
+echo "Charan:: Hello Jyoshna, I am fine, How are you?"
+echo "Jyoshna:: What are you doing recently?"
+echo "Charan:: I am learning shell scripting Jyoshna"sh
