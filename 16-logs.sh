212c183963f6fa1ad018f4da77f2d85672b5ea5e
Charan <charan@gmail.com>
2025-10-04 20:11:40 +0530

    Shell-Roboshop

--- 16-logs.sh
+++ 16-logs.sh
@@ -26,25 +26,25 @@ VALIDATE(){
         echo -e "Installing $2 is $G success $N" | tee -a $LOG_FILE
     fi
 }
-dnf list installed mysql &>>>LOG_FILE
+dnf list installed mysql &>>LOG_FILE
 if [ $? -ne 0 ]; then
-    dnf install mysql -y &>>>LOG_FILE
+    dnf install mysql -y &>>LOG_FILE
     VALIDATE $? "MySQL"
 else
     echo -e "MYSQL already exist... $Y SKIPPING $N" | tee -a $LOG_FILE
 fi
 
-dnf list installed nginx &>>>LOG_FILE
+dnf list installed nginx &>>LOG_FILE
 if [ $? -ne 0 ]; then
-    dnf install nginx -y &>>>LOG_FILE
+    dnf install nginx -y &>>LOG_FILE
     VALIDATE $? "Nginx"
 else
     echo -e "Nginx already exist... $Y SKIPPING $N" | tee -a $LOG_FILE
 fi
 
-dnf list installed python &>>>LOG_FILE
+dnf list installed python &>>LOG_FILE
 if [ $? -ne 0 ]; then
-    dnf install python3 -y &>>>LOG_FILE
+    dnf install python3 -y &>>LOG_FILE
     VALIDATE $? "Python3"
 else
     echo -e "Python3 already exist... $Y SKIPPING $N" | tee -a $LOG_FILE
