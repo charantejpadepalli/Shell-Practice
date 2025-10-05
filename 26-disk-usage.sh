f4ba76ad83463f5da695af1fc19644819bc06eb6
Charan <charan@gmail.com>
2025-10-05 13:22:27 +0530

    Shell-Roboshop

---
+++ 26-disk-usage.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DISK_USAGE=$(df -hT | grep -v Filesystem)
+DISK_THRESHOLD=2 #In projects generally we will keep it as 75
+IP_ADDRESS=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
+MESSAGE=""
+
+while IFS= read -r line
+do
+    USAGE = $(echo $line | awk '{print $6}' | cut -d "%" -f1)
+    PARTITION = $(echo $line | awk '{print $7}')
+    if [ $USAGE -ge $DISK_THRESHOLD ]; then
+        MESSAGE+="High usgae on $PARTITION: $USAGE % <br>"
+    fi
+done <<< $DISK_USAGE
+
+echo -e "Message Body: $MESSAGE"
+
+sh mail.sh "cherryc284@gamil.com" "High Disk Usage Alert" "High Disk Usage" "$MESSAGE" "$IP_ADDRESS" "DevOps Team"
